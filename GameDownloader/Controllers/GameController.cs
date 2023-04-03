73e91b58d311beccadea8d3a1aabb71bf93893d1
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2023-04-03 13:37:33 -0300

    initial commit

---
+++ GameDownloader/Controllers/GameController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GameDownloader.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        // GET: api/<GameController>
+        [HttpGet]
+        public IActionResult GetGameFileForDownload()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
